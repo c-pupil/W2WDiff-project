7dfe5e42b02cc9363653f8a00bacc3db657d46a8
c-pupil <56547062+c-pupil@users.noreply.github.com>
2024-12-30 23:52:49 +0800

    commit

--- src/components/sections/Video.vue
+++ src/components/sections/Video.vue
@@ -1,121 +1,137 @@
-<!--<script>-->
-
-<!--</script>-->
-
-<!--<template>-->
-<!--  <div>-->
-<!--    <el-divider />-->
-
-<!--    <el-row justify="center">-->
-<!--      <h1 class="section-title">Explainer Video</h1>-->
-<!--    </el-row>-->
-
-<!--    &lt;!&ndash; 每个网站的视频的iframe可能不一致，最好在这里手动调整 &ndash;&gt;-->
-<!--    <el-row justify="center">-->
-<!--      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >-->
-<!--        <el-container id="video-container">-->
-<!--          <iframe src="./UCCS_bg.mp4" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>-->
-<!--        </el-container>-->
-<!--      </el-col>-->
-<!--    </el-row>-->
-
-<!--  </div>-->
-<!--</template>-->
-
-<!--<style scoped>-->
-
-<!--#video-container{-->
-<!--  margin: 15px 0px 0px 0px;-->
-<!--}-->
-
-<!--iframe {-->
-<!--  aspect-ratio: 16 / 9;-->
-<!--  height: 100%;-->
-<!--  width: 100%;-->
-<!--}-->
-
-<!--</style>-->
-
-
-
-
 <script>
+import { defineComponent } from 'vue'
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
 
-
-
-export default {
-
-  data() {
-    return {
-
-      // 选择video
-      video_paths: [
-        "./video/UCCS_bg.mp4",
-        "./UCCS_bg.mp4",
-        "../../../../pubic/video/UCCS_bg.mp4",
-        "/public/video/UCCS_bg.mp4",
-
-
-      ],
-    }
+export default defineComponent({
+  components: {
+    VideoPlayer
   }
-}
-
+})
 </script>
 
 <template>
-
-<!--  <div v-for="(videoPath, index) in video_paths" :key="index">-->
-<!--  <p>{{ videoPath }}</p>-->
-<!--  <video controls>-->
-<!--    <source :src="videoPath" type="video/mp4" />-->
-<!--  </video>-->
-<!--</div>-->
-
-
   <div>
     <el-divider />
 
     <el-row justify="center">
-      <h1 class="section-title">Video Comparison</h1>
+      <h1 class="section-title">Explainer Video</h1>
     </el-row>
 
-    <!-- 左右并列播放视频 -->
-    <el-row justify="center" gutter="20">
-      <el-col
-        v-for="(videoPath, index) in video_paths"
-        :key="index"
-        :xs="24"
-        :sm="12"
-        :md="10"
-        :lg="8"
-        :xl="6"
-      >
-        <el-container :id="`video-container-${index}`">
-          <video controls>
-            <source :src="videoPath" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+    <!-- 每个网站的视频的iframe可能不一致，最好在这里手动调整 -->
+
+
+    <!-- 本地视频 -->
+    <el-row justify="center">
+      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
+        <el-container class="video-container">
+          <video-player
+            src="./video/UCCS_bg_enhance.mp4"
+            controls
+            fluid="true"
+            muted="true"
+            :volume="0.5"
+          />
         </el-container>
       </el-col>
     </el-row>
+
   </div>
 </template>
 
-
 <style scoped>
-.section-title {
-  margin-bottom: 20px;
+
+.video-container{
+  margin: 15px 0px 0px 0px;
 }
 
-[id^="video-container"] {
-  margin: 15px 0px;
-}
-
-video {
+iframe {
   aspect-ratio: 16 / 9;
+  height: 100%;
   width: 100%;
-  height: auto;
 }
+
 </style>
+
+
+
+<!--<script>-->
+
+
+
+<!--export default {-->
+
+<!--  data() {-->
+<!--    return {-->
+
+<!--      // 选择video-->
+<!--      video_paths: [-->
+<!--        "./video/UCCS_bg.mp4",-->
+<!--        "./UCCS_bg.mp4",-->
+<!--        "../../../../pubic/video/UCCS_bg.mp4",-->
+<!--        "/public/video/UCCS_bg.mp4",-->
+
+
+<!--      ],-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+
+<!--</script>-->
+
+<!--<template>-->
+
+<!--&lt;!&ndash;  <div v-for="(videoPath, index) in video_paths" :key="index">&ndash;&gt;-->
+<!--&lt;!&ndash;  <p>{{ videoPath }}</p>&ndash;&gt;-->
+<!--&lt;!&ndash;  <video controls>&ndash;&gt;-->
+<!--&lt;!&ndash;    <source :src="videoPath" type="video/mp4" />&ndash;&gt;-->
+<!--&lt;!&ndash;  </video>&ndash;&gt;-->
+<!--&lt;!&ndash;</div>&ndash;&gt;-->
+
+
+<!--  <div>-->
+<!--    <el-divider />-->
+
+<!--    <el-row justify="center">-->
+<!--      <h1 class="section-title">Video Comparison</h1>-->
+<!--    </el-row>-->
+
+<!--    &lt;!&ndash; 左右并列播放视频 &ndash;&gt;-->
+<!--    <el-row justify="center" gutter="20">-->
+<!--      <el-col-->
+<!--        v-for="(videoPath, index) in video_paths"-->
+<!--        :key="index"-->
+<!--        :xs="24"-->
+<!--        :sm="12"-->
+<!--        :md="10"-->
+<!--        :lg="8"-->
+<!--        :xl="6"-->
+<!--      >-->
+<!--        <el-container :id="`video-container-${index}`">-->
+<!--          <video controls>-->
+<!--            <source :src="videoPath" type="video/mp4" />-->
+<!--            Your browser does not support the video tag.-->
+<!--          </video>-->
+<!--        </el-container>-->
+<!--      </el-col>-->
+<!--    </el-row>-->
+<!--  </div>-->
+<!--</template>-->
+
+
+<!--<style scoped>-->
+<!--.section-title {-->
+<!--  margin-bottom: 20px;-->
+<!--}-->
+
+<!--[id^="video-container"] {-->
+<!--  margin: 15px 0px;-->
+<!--}-->
+
+<!--video {-->
+<!--  aspect-ratio: 16 / 9;-->
+<!--  width: 100%;-->
+<!--  height: auto;-->
+<!--}-->
+<!--</style>-->
 
