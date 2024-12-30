b79ed151a84a166b2861df9edd3dba9adeb4f2b0
c-pupil <56547062+c-pupil@users.noreply.github.com>
2024-12-30 17:22:04 +0800

    commit

--- src/components/sections/Video.vue
+++ src/components/sections/Video.vue
@@ -14,7 +14,7 @@
 <!--    <el-row justify="center">-->
 <!--      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >-->
 <!--        <el-container id="video-container">-->
-<!--          <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=802517980&bvid=BV1qy4y1x7VV&cid=325180260&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>-->
+<!--          <iframe src="./UCCS_bg.mp4" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>-->
 <!--        </el-container>-->
 <!--      </el-col>-->
 <!--    </el-row>-->
@@ -37,69 +37,42 @@
 <!--</style>-->
 
 
-<!--<template>-->
-<!--  <div>-->
-<!--    <el-divider />-->
 
-<!--    <el-row justify="center">-->
-<!--      <h1 class="section-title">Explainer Video</h1>-->
-<!--    </el-row>-->
 
-<!--    <el-row justify="center">-->
-<!--      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10">-->
-<!--        <el-container id="video-container">-->
-<!--          <div class="video-wrapper">-->
-<!--            <video ref="video1" controls @loadedmetadata="setVideoDimensions">-->
-<!--              <source src="./video/UCCS_bg.mp4" type="video/mp4">-->
-<!--              Your browser does not support the video tag.-->
-<!--            </video>-->
-<!--            <video ref="video2" controls @loadedmetadata="setVideoDimensions">-->
-<!--              <source src="./video/UCCS_bg_enhance.mp4" type="video/mp4">-->
-<!--              Your browser does not support the video tag.-->
-<!--            </video>-->
-<!--          </div>-->
-<!--        </el-container>-->
-<!--      </el-col>-->
-<!--    </el-row>-->
-<!--  </div>-->
-<!--</template>-->
+<script>
 
-<!--<script>-->
-<!--export default {-->
-<!--  methods: {-->
-<!--    setVideoDimensions() {-->
-<!--      const video1 = this.$refs.video1;-->
-<!--      const video2 = this.$refs.video2;-->
 
-<!--      // 设置视频的高度和宽度-->
-<!--      const height = Math.max(video1.videoHeight, video2.videoHeight);-->
-<!--      const width = video1.videoWidth + video2.videoWidth;-->
 
-<!--      video1.style.height = `${height}px`;-->
-<!--      video2.style.height = `${height}px`;-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
+export default {
 
-<!--<style scoped>-->
-<!--#video-container {-->
-<!--  margin: 15px 0;-->
-<!--}-->
+  data() {
+    return {
 
-<!--.video-wrapper {-->
-<!--  display: flex;-->
-<!--  justify-content: space-between;-->
-<!--}-->
+      // 选择video
+      video_paths: [
+        "./video/UCCS_bg.mp4",
+        "./UCCS_bg.mp4",
+        "../../../../pubic/video/UCCS_bg.mp4",
+        "/public/video/UCCS_bg.mp4",
 
-<!--video {-->
-<!--  width: 50%; /* 每个视频占一半宽度 */-->
-<!--  height: auto; /* 高度自动调整 */-->
-<!--}-->
-<!--</style>-->
 
+      ],
+    }
+  }
+}
+
+</script>
 
 <template>
+
+<!--  <div v-for="(videoPath, index) in video_paths" :key="index">-->
+<!--  <p>{{ videoPath }}</p>-->
+<!--  <video controls>-->
+<!--    <source :src="videoPath" type="video/mp4" />-->
+<!--  </video>-->
+<!--</div>-->
+
+
   <div>
     <el-divider />
 
@@ -109,18 +82,18 @@
 
     <!-- 左右并列播放视频 -->
     <el-row justify="center" gutter="20">
-      <el-col :xs="24" :sm="12" :md="10" :lg="8" :xl="6">
-        <el-container id="video-container-left">
+      <el-col
+        v-for="(videoPath, index) in video_paths"
+        :key="index"
+        :xs="24"
+        :sm="12"
+        :md="10"
+        :lg="8"
+        :xl="6"
+      >
+        <el-container :id="`video-container-${index}`">
           <video controls>
-            <source src="./video/UCCS_bg.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </el-container>
-      </el-col>
-      <el-col :xs="24" :sm="12" :md="10" :lg="8" :xl="6">
-        <el-container id="video-container-right">
-          <video controls>
-            <source src="./video/UCCS_bg_enhance.mp4" type="video/mp4" />
+            <source :src="videoPath" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </el-container>
@@ -129,13 +102,13 @@
   </div>
 </template>
 
+
 <style scoped>
 .section-title {
   margin-bottom: 20px;
 }
 
-#video-container-left,
-#video-container-right {
+[id^="video-container"] {
   margin: 15px 0px;
 }
 
@@ -145,3 +118,4 @@ video {
   height: auto;
 }
 </style>
+
